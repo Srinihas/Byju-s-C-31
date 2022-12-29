580d71f57f4122f4560cbc83b72cc39bed76353e
Srinihas <115796290+Srinihas@users.noreply.github.com>
2022-12-29 15:12:30 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -90,7 +90,7 @@ function keyPressed() {
     var posX = playerArcher.body.position.x;
     var posY = playerArcher.body.position.y;
     var angle = playerArcher.body.angle+PI/2;
-    var arrow = new PlayerArrow(posX, posY, 100, 10);
+    var arrow = new Playerarrow(posX, posY, 100, 10);
     arrow.trajectory = [];
     Matter.Body.setAngle(arrow.body, angle);
     playerArrows.push(arrow);
